6e3c257897f5b61b387fb64c0541b4b9c17a87c6
Michealsun Baluyos <michael.baluyos@gmail.com>
2015-04-08 19:29:01 +1000

    Bishop piece returns movable positions

--- src/chess/prototype/composite/Bishop.java
+++ src/chess/prototype/composite/Bishop.java
@@ -16,7 +16,72 @@ public class Bishop extends Piece {
 	
 	@Override
 	public int[] getMovablePositions(int currPos) {
-		return new int[]{ 1, 2 };
+		ArrayList<Integer> positions = new ArrayList<Integer>();
+		if(currPos > 0 && currPos < 36) {
+			int x = (currPos % 6);
+			positions.add(currPos);
+
+			// For all down left
+			int movable = currPos;
+			int currX = x;
+			boolean isLeft = true;
+			while(isLeft) {
+				movable += 5;
+				int checkLeftX = movable % 6;
+				isLeft = (checkLeftX < currX) && (movable < 36);
+				if(isLeft) {
+					positions.add(movable);
+					currX = checkLeftX;
+				}
+			}
+			// For all up right
+			movable = currPos;
+			currX = x;
+			boolean isRight = true;
+			while(isRight) {
+				movable -= 5;
+				int checkRightX = movable % 6;
+				isRight = (checkRightX > currX) && (movable >= 0);
+				if(isRight) {
+					positions.add(movable);
+					currX = checkRightX;
+				}
+			}
+			// For all down right
+			movable = currPos;
+			currX = x;
+			isRight = true;
+			while(isRight) {
+				movable += 7;
+				int checkRightX = movable % 6;
+				isRight = (checkRightX > currX) && (movable < 36);
+				if(isRight) {
+					positions.add(movable);
+					currX = checkRightX;
+				}
+			}
+			// For all up left
+			movable = currPos;
+			currX = x;
+			isLeft = true;
+			while(isLeft) {
+				movable -= 7;
+				int checkLeftX = movable % 6;
+				isLeft = (checkLeftX < currX) && (movable >= 0);
+				if(isLeft) {
+					positions.add(movable);
+					currX = checkLeftX;
+				}
+			}
+		}
+		
+		int[] ret = new int[positions.size()];
+		int i = 0;
+		for(Integer pos: positions) {
+			ret[i++] = pos;
+		}
+		
+		return ret;
 	}
 
 }
