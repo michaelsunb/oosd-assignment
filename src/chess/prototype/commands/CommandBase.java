044e5ca473e3a918b0f5a4cad5d399ce1391e72a
Michealsun Baluyos <michael.baluyos@gmail.com>
2015-05-15 20:04:02 +1000

    revert back to using the singleton on the CommandBase

--- src/chess/prototype/commands/CommandBase.java
+++ src/chess/prototype/commands/CommandBase.java
@@ -2,33 +2,30 @@ package chess.prototype.commands;
 
 import chess.core.Game;
 import chess.core.IBoard;
+import chess.core.Piece;
+import chess.core.Player;
+import chess.mvc.models.PieceMovedEvent;
 import chess.prototype.observer.ChessEvent;
 import chess.prototype.observer.ChessEventDispatcher;
 import chess.prototype.observer.IObserver;
 
 public abstract class CommandBase implements IObserver {
-	private final Game game;
-	private final IBoard board;
-	private final ChessEventDispatcher eventMgr;
 	
 	public CommandBase() {
-		this.game = Game.getInstance();
-		this.board = game.getBoardInstance();
-		this.eventMgr = ChessEventDispatcher.getInstance();
-	}
-	
-	public Game getGame() {
-		return game;
-	}
-	
-	public IBoard getBoard () {
-		return board;
-	}
-	
-	public ChessEventDispatcher eventMgr() {
-		return eventMgr; 
 	}
 	
 	@Override
 	public abstract void update(ChessEvent event);
+	
+	public Game getGame() {
+		return Game.getInstance();
+	}
+	
+	public IBoard getBoard () {
+		return Game.getInstance().getBoardInstance();
+	}
+	
+	public ChessEventDispatcher eventMgr() {
+		return ChessEventDispatcher.getInstance(); 
+	}
 }
