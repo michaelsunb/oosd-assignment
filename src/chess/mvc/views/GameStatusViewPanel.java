9498918c1b72381e4ba4b2ce2d95a7095e368dd7
Chorn Sokun <chornsokun@gmail.com>
2015-05-15 21:41:22 +1000

    Sample view (self contain)

--- src/chess/mvc/views/GameStatusViewPanel.java
+++ src/chess/mvc/views/GameStatusViewPanel.java
@@ -1,13 +1,12 @@
 package chess.mvc.views;
 
-import java.awt.Color;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Rectangle;
 
 import javax.swing.*;
 
-import chess.mvc.models.GameStatusEvent;
-import chess.mvc.models.UpdateUIEvent;
+import chess.core.Game;
 import chess.prototype.observer.ChessEvent;
 import chess.prototype.observer.IObserver;
 
@@ -15,57 +14,83 @@ import chess.prototype.observer.IObserver;
  * A panel which render game status
  */
 public class GameStatusViewPanel extends JPanel implements IObserver {
-	private int numberOfMoves = 0;
-	private int score = 0;
-
+	private JLabel[] moves; 
+	private JLabel[] scores;
+	private JLabel timeCounter;
+	private static final int PLAYER_ONE = 0;
+	private static final int PLAYER_TWO = 1;
+	
 	public GameStatusViewPanel() {
-		this.setLayout(new GridLayout (0, 1));
+		JPanel statusPane = playerStats();
+		this.add(statusPane);
 
-		createStatus(1);
-		createStatus(2);
-		
+		this.revalidate();
 		this.setBounds(new Rectangle(60, 60));
 	}
 
-	private void createStatus(int player) {
-		JLabel playerLabel = new JLabel("Player " + player);
-		this.add(playerLabel);
 
-		JLabel playerMovedLabel = new JLabel("Moves " + numberOfMoves);
-		this.add(playerMovedLabel);
+	private JPanel playerStats() {
+		JPanel statusPane = new JPanel();
+		statusPane.setLayout(new GridBagLayout ());
 
-		JLabel playerScoredLabel = new JLabel("Scores " + score);
-		this.add(playerScoredLabel);
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		c.gridy = 0;
+		
+		c.anchor = GridBagConstraints.LINE_START;
+		// player one
+		statusPane.add(new JLabel("Player One"), c);
+		c.anchor = GridBagConstraints.LINE_END;
+		c.gridy++;
+		statusPane.add(new JLabel("Move: "), c);
+		
+		c.gridy++;
+		statusPane.add(new JLabel("Score: "), c);
 
-		this.revalidate();
+		// player two
+		c.gridy++;
+		statusPane.add(new JLabel("Player Two"), c);
+		c.anchor = GridBagConstraints.LINE_END;
+		c.gridy++;
+		statusPane.add(new JLabel("Move: "), c);
+		
+		c.gridy++;
+		statusPane.add(new JLabel("Score: "), c);
+		
+		// score & move
+		moves = new JLabel[] {
+				new JLabel("0"),
+				new JLabel("0")
+		};
+		scores = new JLabel[] {
+				new JLabel("0"),
+				new JLabel("0")
+		};
+		
+		c.gridx = 1;
+		c.gridy = 1;
+		statusPane.add(moves[0], c);
+		c.gridy++;
+		statusPane.add(scores[0], c);
+
+		c.gridy += 2;
+		statusPane.add(moves[1], c);
+		c.gridy++;
+		statusPane.add(scores[1], c);
+		return statusPane;
 	}
+
 
 	@Override
 	public void update(ChessEvent event) {
-		if (event instanceof GameStatusEvent) {
-			updateGameStatus((GameStatusEvent) event);
-		} else if (event instanceof UpdateUIEvent) {
-			update((UpdateUIEvent)event);
-		}
-	}
-
-	public void update(UpdateUIEvent event) {
-		numberOfMoves = event.getgame().getMaxMoves() - numberOfMoves;
-		score += event.getgame().getCurrentPlayer().getScore();
-
-		this.removeAll();
-		createStatus(2);
-		createStatus(1);
-	}
-
-	private void updateGameStatus(GameStatusEvent event) {
-		numberOfMoves = event.getNumberOfMoves() - numberOfMoves;
-		score += event.getTargetPiece().getScore();
-
-		if (event.getTargetPiece().getColour() == Color.WHITE) {
-			createStatus(2);
-		} else {
-			createStatus(1);
-		}
+		// NOTE in fact we can get info from Game.getInstance();
+		// all we need is someone call this method
+		Game g = Game.getInstance();
+		
+		moves[PLAYER_ONE].setText("" + g.getPlayer(1).getNumberOfMove());
+		scores[PLAYER_ONE].setText("" + g.getPlayer(1).getScore());
+		
+		moves[PLAYER_TWO].setText("" + g.getPlayer(2).getNumberOfMove());
+		scores[PLAYER_TWO].setText("" + g.getPlayer(2).getScore());
 	}
 }
